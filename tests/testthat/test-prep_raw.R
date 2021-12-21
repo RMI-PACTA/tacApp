ae0af6a194028a97910b49ccc10951e090b4fe31
Mauro Lepore <maurolepore@gmail.com>
2021-12-21 10:53:01 -0300

    Use tweaked data

--- tests/testthat/test-prep_raw.R
+++ tests/testthat/test-prep_raw.R
@@ -2,7 +2,8 @@ test_that("outputs the same", {
   skip_if_not_installed("tacAppPrivateData")
 
   path <- private_path("nuclear_coal.csv")
-  data <- readr::read_csv(path, show_col_types = FALSE)
+  data <- readr::read_csv(path, show_col_types = FALSE) %>%
+    tweak()
 
   out <- prep_raw(data, company_id = 6736, technology = "coal")
   expect_snapshot(out)
@@ -12,7 +13,8 @@ test_that("with invalid id errors gracefully", {
   skip_if_not_installed("tacAppPrivateData")
 
   path <- private_path("nuclear_coal.csv")
-  data <- readr::read_csv(path, show_col_types = FALSE)
+  data <- readr::read_csv(path, show_col_types = FALSE) %>%
+    tweak()
 
   expect_snapshot_error(prep_raw(data, company_id = 1, technology = "coal"))
 })
