6eab147d503aac0ac956265cb49cb992bc8de19b
Mauro Lepore <maurolepore@gmail.com>
2021-12-21 10:59:21 -0300

    Tidy

--- R/imports.R
+++ R/imports.R
@@ -1,14 +1,14 @@
-#' @importFrom dplyr arrange case_when filter group_by filter if_else mutate
-#' @import shiny
 #' @import ggplot2
+#' @import shiny
+#' @importFrom dplyr arrange case_when filter group_by filter if_else mutate
 #' @importFrom dplyr select summarise mutate relocate pull bind_rows
-#' @importFrom tibble enframe
-#' @importFrom tidyr unnest
+#' @importFrom DT renderDT DTOutput
 #' @importFrom readr read_csv write_csv
 #' @importFrom rlang %||% .data .env sym
 #' @importFrom r2dii.plot theme_2dii
+#' @importFrom tibble enframe
+#' @importFrom tidyr unnest
 #' @importFrom utils head
-#' @importFrom DT renderDT DTOutput
 NULL
 
 globalVariables(c(
