29aa9bef8387ac07d7c6ccca1bba0127f255be4f
Mauro Lepore <maurolepore@gmail.com>
2021-12-23 08:55:20 -0300

    Round percent values in summary table

--- R/summarize_change.R
+++ R/summarize_change.R
@@ -40,3 +40,42 @@ summarize1change <- function(.x) {
 category_value <- function(data, category) {
   filter(data, .data$category == .env$category)$value
 }
+
+#' @examples
+#' x <- c("real", "virtual")
+#' nms <- c(
+#'   "total_change",
+#'   paste0("percent_", x),
+#'   paste0(x, "_change")
+#' )
+#'
+#' nms
+#' format_summary_names(nms)
+#' @noRd
+format_summary_names <- function(nms) {
+  nms <- gsub("_", " ", nms)
+  nms <- tools::toTitleCase(nms)
+  # styler: off
+  nms <- case_when(
+    grepl("Percent", nms) ~ add_unit(nms, "%"),
+    grepl("Change", nms)  ~ add_unit(nms, "GW"),
+    TRUE                  ~ nms
+  )
+  # styler: on
+  nms <- gsub("Change ", "", nms)
+  nms <- gsub("Percent ", "", nms)
+  nms
+}
+
+#' @examples
+#' x <- c("foo", "bar")
+#' add_unit(x, "%")
+#' add_unit(x, "GW")
+#' @noRd
+add_unit <- function(x, unit) {
+  sprintf("%s (%s)", x, unit)
+}
+
+round_percent_columns <- function(data) {
+  mutate(data, across(matches("percent"), ~ as.integer(round(.x))))
+}
