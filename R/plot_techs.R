5781b76f16e387c1abb7f1cc757be2ab26dbb90f
Mauro Lepore <maurolepore@gmail.com>
2021-12-23 09:17:29 -0300

    Refactor

--- R/plot_techs.R
+++ R/plot_techs.R
@@ -1,8 +1,5 @@
 plot_techs <- function(data) {
   plot_waterfall(data) +
-    facet_wrap(vars(.data$technology)) +
-    xlab("") +
-    ylab("Power Capacity (GW)") +
-    # TODO: Ask where to get company name. The data seems to have only company id
+    labs(x = "", y = "Power Capacity (GW)", title = unique(data$technology)) +
     theme(axis.text = element_text(angle = 90, hjust = 1))
 }
