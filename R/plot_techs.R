408e0e101eed4a255cd1d1021114f21d3a4c04f9
Mauro Lepore <maurolepore@gmail.com>
2021-12-23 20:00:30 -0300

    Update

--- R/plot_techs.R
+++ R/plot_techs.R
@@ -1,5 +1,15 @@
-plot_techs <- function(data) {
-  plot_waterfall(data) +
+plot_techs <- function(data, fmt_category = format_category) {
+  p <- plot_waterfall(data) +
     labs(x = "", y = "Power Capacity (GW)", title = unique(data$technology)) +
     theme(axis.text = element_text(angle = 90, hjust = 1))
+
+  suppressMessages(
+    p + scale_x_discrete(labels = fmt_category(data$category))
+  )
+}
+
+format_category <- function(x) {
+  x <- gsub("before", "year 0", x)
+  x <- gsub("after", "year 1", x)
+  tools::toTitleCase(x)
 }
