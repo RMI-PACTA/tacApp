26000590d47ac5e8a8554eb23b95da4a4af350fb
Mauro Lepore <maurolepore@gmail.com>
2021-12-27 08:59:52 -0300

    Restore right y-axis

--- R/plot_techs.R
+++ R/plot_techs.R
@@ -4,9 +4,7 @@ plot_techs <- function(data, ...) {
     theme(axis.text = element_text(angle = 90, hjust = 1))
 
   p <- suppressMessages(
-    p +
-      scale_x_discrete(labels = format_category(data$category)) +
-      scale_y_continuous(sec.axis = waiver())
+    p + scale_x_discrete(labels = format_category(data$category))
   )
 
   p + theme(...)
